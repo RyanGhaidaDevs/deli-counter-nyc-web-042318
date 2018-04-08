a4445d94f71e6ea406fc89b3a471d46ac7b84d42
Ryan Ghaida <ryaboug@gmail.com>
2018-04-08 19:19:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line (array)
+
+  i = 1
+  arr = ["The line is currently:"]
+
+ if array.length == 0
+   puts "The line is currently empty."
+
+  else array.collect do |name|
+
+   arr << "#{i}. " +  "#{name}"
+
+   i += 1
+
+  end
+  puts arr.join(" ")
+  end
+
+ end
