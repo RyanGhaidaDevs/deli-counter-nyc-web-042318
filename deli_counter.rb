5ebbe20ed4c72d4a1a35f7873983aef003d75a95
Ryan Ghaida <ryaboug@gmail.com>
2018-04-08 19:42:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,24 +20,25 @@ def line (array)
 
 
  def take_a_number (katz_deli, name)
-
-   arr2 = []
-    i = 0
-
-
-
-   katz_deli << name
-
-   while i < katz_deli.length
-
-    arr2.push("Welcome, #{name}. You are number #{i+1} in line.")
-
-     i+= 1
-   end
-
+   
+   arr2 = [] 
+    i = 0 
+   
+    
+    
+   katz_deli << name 
+   
+   while i < katz_deli.length 
+   
+    arr2.push("Welcome, #{name}. You are number #{katz_deli.length} in line.")
+   
+     i+= 1 
+     
+   end 
+   
    puts arr2.join(" ")
-
+   
   katz_deli
-
-
+ 
+ 
  end 
